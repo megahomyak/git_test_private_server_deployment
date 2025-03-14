8ad83fafdc3cb607be18cf11226ff73d917c6279
megahomyak <g.megahomyak@gmail.com>
2025-03-14 16:07:44 +0500

    Adding all tha shite. I'll see how multiple remotes work next

--- deployment_preparation.bash
+++ deployment_preparation.bash
@@ -3,8 +3,7 @@
 ssh orange << EOF
 mkdir -p git_test_private_server_deployment
 cd git_test_private_server_deployment
-if [ ! -d .git ]
+if [ ! -d .git ]; then
     git init --bare
-    git remote set-url origin --push --add orange:git_test_private_server_deployment
 fi
 EOF
